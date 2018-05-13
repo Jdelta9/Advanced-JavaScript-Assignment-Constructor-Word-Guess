5d26454820cc0b0701412d2c2f092f81a838ba1d
Jdelta9 <35478858+Jdelta9@users.noreply.github.com>
2018-05-13 15:34:01 -0700

    Update word.js

--- word.js
+++ word.js
@@ -1,1 +1,29 @@
+let letter = require('./letter.js')
+let wordBank = ['Honda', 'Toyota', 'Lexus', 'Acura', 'BMW', 'Infinity', 'Suburu'];
+
+function Word () {
+    this.letterObjectArray = ['A','B','C','D','E','F','G','H','R','S','T','U','V','W','X','Y','Z'];
+    this.chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+    for (let i = 0; i < this.wordBank.length; i++) {
+        let temp = new letter(this.letterObjectArray[i]);
+        this.letterObjectArray.push(temp);
+    }
+    console.log('test letterCheck', this.letterObjectArray[2].letterChecker('a'));
+}
+
+Word.prototype.print = function() {
+    let printWord = '';
+    for (let i = 0; this.letterObjectArray.length; i++) {
+        printWord += this.letterObjectArray[i] + '';
+    }
+}
+
+Word.prototype.userChoice = function(input) {
+    for (let i = 0; this.letterObjectArray.length; i++) {
+        this.letterObjectArray[i].guess(input);
+    }
+}
+
+module.exports = Word;
 
