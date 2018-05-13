d99fc1a91ba8907639770f103e97fe457a5c7152
Jdelta9 <35478858+Jdelta9@users.noreply.github.com>
2018-05-13 15:30:20 -0700

    Update letter.js

--- letter.js
+++ letter.js
@@ -1,1 +1,28 @@
+function Letter (word) {
+    this.letter = word;
+    this.letterGuessed = false;
+};
+
+Letter.prototype.characterDisplay = function() {
+    if (this.letter === true){
+        return this.letter;
+    } else {
+      return "-"
+    }
+}
+
+Letter.prototype.letterChecker = function(guess) {
+    if (guess === this.letter) {
+        this.letterGuessed = true;
+    } else {
+        this.letterGuessed = false;
+    }
+    
+    return this.letterGuessed;
+    console.log(this.letterGuessed);
+}
+
+
+module.exports = Letter;
+
 
