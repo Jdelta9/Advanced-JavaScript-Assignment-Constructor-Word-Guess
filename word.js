2e71e26fab089147a068e8c31225bc175dc58bbd
Jdelta9 <35478858+Jdelta9@users.noreply.github.com>
2018-05-13 16:11:52 -0700

    Update word.js

--- word.js
+++ word.js
@@ -1,26 +1,26 @@
-let letter = require('./letter.js')
-let wordBank = ['Honda', 'Toyota', 'Lexus', 'Acura', 'BMW', 'Infinity', 'Suburu'];
+var letter = require('./letter.js')
+var wordBank = ['Honda', 'Toyota', 'Lexus', 'Acura', 'BMW', 'Infinity', 'Suburu'];
 
 function Word () {
     this.letterObjectArray = ['A','B','C','D','E','F','G','H','R','S','T','U','V','W','X','Y','Z'];
     this.chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 
-    for (let i = 0; i < this.wordBank.length; i++) {
-        let temp = new letter(this.letterObjectArray[i]);
+    for (var i = 0; i < this.wordBank.length; i++) {
+        var temp = new letter(this.letterObjectArray[i]);
         this.letterObjectArray.push(temp);
     }
     console.log('test letterCheck', this.letterObjectArray[2].letterChecker('a'));
 }
 
 Word.prototype.print = function() {
-    let printWord = '';
+    var printWord = '';
     for (let i = 0; this.letterObjectArray.length; i++) {
         printWord += this.letterObjectArray[i] + '';
     }
 }
 
 Word.prototype.userChoice = function(input) {
-    for (let i = 0; this.letterObjectArray.length; i++) {
+    for (var i = 0; this.letterObjectArray.length; i++) {
         this.letterObjectArray[i].guess(input);
     }
 }
