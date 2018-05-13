aaeafe22fdaefa56a4bc61618ef7cd39baaa0195
Jdelta9 <35478858+Jdelta9@users.noreply.github.com>
2018-05-13 15:21:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
+let Letter = require('./letter.js');
+let inquirer = require('inquirer');
+let Word = require('./word.js')
+//console.log(Letter);
 
+
+let wordBank = ['Honda', 'Toyota', 'Lexus', 'Acura', 'BMW', 'Infinity', 'Suburu'];
+
+//this.chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+//You need an array for guessed correct and incorrect letters
+let correctLetters = [];
+let incorrectLetter = [];
+
+
+//for underscores to appear will be used in a function
+let underScore = "_"
+
+
+// Guesses user has in the game
+let guessesLeft = 5;
+
+//randomly selects a word and uses a constructor to store it
+// function playGame() {
+// let wordChoice = new Word(chosenWord);
+// Word.underScoreMake();
+// }
+
+//This is to grab a random word
+// function grabRandomWord() {
+//     let rand = wordBank[Math.floor(Math.random() * wordBank.length)];
+//     let randomSelection = wordBank[rand];
+//     if(chosenWord.indexOf(randomWord) === -1) {
+//       return randomWord;
+//     } else {
+//     grabRandomWord()
+//     gameGuessing();
+//     playGame();
+//     }
+//   }
+
+// function gameGuessing (){
+//   if (guessesLeft = 5) {
+//         inquirer.prompt([
+//             {
+//                 type: "input",
+//                message: 'Guess a letter:',
+//               name: "guess"
+//            }
+//     ]).then(function(answer){
+//            letterValidate = this.letterChecker(guess);
+//         if (guessesLeft === 0) {
+               //console.log('');
+//                 console.log("Youre guesses were incorrect" + ansewer.guessedLetter + "Try Again!");
+//                 guessesLeft--;
+//             } else {
+//         console.log(this.letterChecker);
+            
+//             if (guesses > 0 && correct) {
+//                 letterValidate = this.letterChecker(guess);
+//                //console.log('');
+//                 console.log("Your guess is correct"+ answer.guessesLetter + "Good Job!");
+//                 inquire();
+//             } else {
+//                console.log("GuessesLeft" + guessesLeft);      
+               
+//         } console.log(answers);
+//          inquirer();
+//     }
+    
+//  });
